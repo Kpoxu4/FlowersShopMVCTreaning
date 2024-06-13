1528be713b7ec1d1c81c842c9fbf59d3cf1503fe
TatianaNai <tatyana_tat333@mail.ru>
2024-06-13 19:12:00 +0300

    Add login user

---
+++ FlowersShopMVCTreaning/Models/AuthUser/AuthUserViewModel.cs
@@ -0,0 +1,9 @@
+namespace FlowersShopMVCTraining.Models.AuthUser
+{
+    public class AuthUserViewModel
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+}

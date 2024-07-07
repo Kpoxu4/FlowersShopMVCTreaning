3d9ad35845251745fd9f438d38f7992b19ad96a9
Kpoxu4 <159006608+Kpoxu4@users.noreply.github.com>
2024-07-07 18:40:39 +0300

    add 5 card

--- FlowersShopMVCTreaningRepository/Seed.cs
+++ FlowersShopMVCTreaningRepository/Seed.cs
@@ -7,7 +7,7 @@ namespace FlowersShopMVCTraining.Repository
 {
     public class Seed
     {
-        
+
         public void Fill(IServiceProvider serviceProvider, string webRootPath)
         {
             using var service = serviceProvider.CreateScope();
@@ -39,36 +39,241 @@ namespace FlowersShopMVCTraining.Repository
 
             if (!shopCardRepository.Any())
             {
-               CreateShopCard(
-               shopCardRepository,
-               productDescriptionRepository,
-               webRootPath,
-               "Весенний взгляд",
-               "Bouquets1",
-               "Bouquets",
-               2000,
-               30,
-               ProductFeatures.DealOfDay | ProductFeatures.Bestseller | ProductFeatures.NewArrival,
-               "Этот букет состоит из нежных розово-белых альстромерий, которые создают гармоничное " +
-               "сочетание цветов. Зеленые листья и веточки добавляют свежести и живости." +
-               " Букет аккуратно упакован в розовую бумагу, придавая ему элегантный вид.");
+                #region Catalog Bouquets
+                
+                CreateShopCard(
+                shopCardRepository,
+                productDescriptionRepository,
+                webRootPath,
+                "Весенний взгляд",
+                "Bouquets1",
+                "Bouquets",
+                2000,
+                30,
+                ProductFeatures.DealOfDay | ProductFeatures.Bestseller | ProductFeatures.NewArrival,
+                "Этот букет состоит из нежных розово-белых альстромерий, которые создают гармоничное " +
+                "сочетание цветов. Зеленые листья и веточки добавляют свежести и живости." +
+                " Букет аккуратно упакован в розовую бумагу, придавая ему элегантный вид.");
+
+                CreateShopCard(
+                shopCardRepository,
+                productDescriptionRepository,
+                webRootPath,
+                "Букет Летней Радости",
+                "Bouquets2",
+                "Bouquets",
+                1500,
+                0,
+                ProductFeatures.DealOfDay,
+                "Описание продукта: Букет “Летней Радости” — это яркое и теплое сочетание оранжевых гербер и желтых роз. " +
+                "Он напоминает о летнем солнечном дне, когда цветы расцветают во всей своей красе. " +
+                "Этот букет подарит радость и тепло вашим близким.");
 
                 CreateShopCard(
                 shopCardRepository,
                 productDescriptionRepository,
                 webRootPath,
                 "Букет Нежной Гармонии",
-                "Bouquets2",
+                "Bouquets3",
+                "Bouquets",
+                2500,
+                50,
+                ProductFeatures.DealOfDay | ProductFeatures.Bestseller | ProductFeatures.NewArrival,
+                "Букет “Нежной Гармонии” — это воплощение нежности и умиротворения. " +
+                "В нем сочетаются пушистые хлопковые шарики, нежные маргаритки и лавандовые цветы, " +
+                "создавая атмосферу спокойствия и уюта. Этот букет идеально подходит для подарка или для украшения вашего дома. " +
+                "Позвольте ему принести в вашу жизнь немного мягкости и радости.");
+
+                CreateShopCard(
+                shopCardRepository,
+                productDescriptionRepository,
+                webRootPath,
+                "Букет Летней Россыпи",
+                "Bouquets4",
+                "Bouquets",
+                2200,
+                10,
+                ProductFeatures.NewArrival,
+                "Букет “Летней Россыпи” — это яркое и вдохновляющее сочетание желтых тюльпанов, " +
+                "белых декоративных цветов и веточек с нежными бутонами. " +
+                "Он упакован в бумагу сине-белого цвета и завязан белой атласной лентой. " +
+                "Этот букет приносит радость и свежесть, идеально подходит для подарка или для украшения вашего дома.");
+
+                CreateShopCard(
+                shopCardRepository,
+                productDescriptionRepository,
+                webRootPath,
+                "Букет Летнего Заката",
+                "Bouquets5",
+                "Bouquets",
+                2200,
+                10,
+                ProductFeatures.DealOfDay | ProductFeatures.Bestseller | ProductFeatures.NewArrival,
+                "Букет “Летнего Заката” — это воплощение тепла и ярких эмоций. " +
+                "В нем сочетаются оранжевые розы, желтые герберы и зелень, " +
+                "создавая атмосферу летнего вечера. Этот букет идеально подходит для подарка или для украшения вашего дома. " +
+                "Позвольте ему принести в вашу жизнь краски и радость.");
+
+                #endregion
+
+                #region ForWhom
+
+                CreateShopCard(
+                shopCardRepository,
+                productDescriptionRepository,
+                webRootPath,
+                "Цветочная симфония",
+                "ForWhom6",
+                "Bouquets",
+                2000,
+                0,
+                ProductFeatures.DealOfDay | ProductFeatures.Bestseller | ProductFeatures.NewArrival,
+                "Этот букет состоит из нежных розово-белых альстромерий, " +
+                "которые создают гармоничное сочетание цветов. " +
+                "Зеленые листья и веточки добавляют свежести и живости. " +
+                "Букет аккуратно упакован в розовую бумагу, придавая ему элегантный вид. " +
+                "Такой букет станет отличным подарком для любого торжества или просто чтобы поднять настроение любимому человеку.");
+
+                CreateShopCard(
+                shopCardRepository,
+                productDescriptionRepository,
+                webRootPath,
+                "Сердце страсти",
+                "ForWhom7",
                 "Bouquets",
                 1500,
                 0,
-                ProductFeatures.DealOfDay | ProductFeatures.NewArrival,
-                "Букет “Нежной Гармонии” — это воплощение нежности и умиротворения. " +
-                "В нем сочетаются пушистые хлопковые шарики, нежные маргаритки и" +
-                " лавандовые цветы, создавая атмосферу спокойствия и уюта. Этот букет " +
-                "идеально подходит для подарка или для украшения вашего дома. " +
-                "Позвольте ему принести в вашу жизнь немного мягкости и радости.");
+                ProductFeatures.DealOfDay,
+                "В этом букете красные розы собраны в форме сердца, " +
+                "что делает его идеальным подарком для выражения самых теплых и страстных чувств. " +
+                "Цветы аккуратно упакованы в двухцветную бумагу и перевязаны лентой, " +
+                "подчеркивающей яркость и романтичность композиции. " +
+                "«Сердце страсти» станет великолепным подарком на День святого Валентина, юбилей или любое другое важное событие.");
 
+                CreateShopCard(
+                shopCardRepository,
+                productDescriptionRepository,
+                webRootPath,
+                "Страстное Признание",
+                "ForWhom8",
+                "Bouquets",
+                2500,
+                10,
+                ProductFeatures.DealOfDay | ProductFeatures.Bestseller | ProductFeatures.NewArrival,
+                "Великолепная композиция из ярко-красных роз, элегантно завернутая в красно-белую бумагу. " +
+                "Этот букет излучает любовь и страсть, " +
+                "делая его идеальным подарком для выражения глубоких чувств.");
+
+                CreateShopCard(
+                shopCardRepository,
+                productDescriptionRepository,
+                webRootPath,
+                "Романтические Объятия",
+                "ForWhom9",
+                "Bouquets",
+                2200,
+                20,
+                ProductFeatures.NewArrival,
+                "Очаровательный букет в форме сердца из красных роз, " +
+                "дополненный натуральной зеленью и мешковиной. " +
+                "Эта очаровательная композиция символизирует любовь и романтику, " +
+                "идеально подходящая для особого человека.");
+
+                CreateShopCard(
+                shopCardRepository,
+                productDescriptionRepository,
+                webRootPath,
+                "Весенняя Гармония",
+                "ForWhom10",
+                "Bouquets",
+                2200,
+                30,
+                ProductFeatures.DealOfDay | ProductFeatures.Bestseller | ProductFeatures.NewArrival,
+                "Этот изысканный букет сочетает в себе нежные розовые гортензии и разноцветные гипсофилы. " +
+                "Упакованный в элегантную розовую бумагу, он приносит ощущение свежести и жизненной силы, " +
+                "идеально подходя для любого случая.");
+
+                #endregion 
+
+                #region HolidayCollection // TODO НАЗВАНИЕ и Описание дял следуших обьектов
+
+
+                CreateShopCard(
+                shopCardRepository,
+                productDescriptionRepository,
+                webRootPath,
+                "Цветочная симфония",
+                "HolidayCollection11",
+                "HolidayCollection",
+                2200,
+                10,
+                ProductFeatures.DealOfDay | ProductFeatures.Bestseller | ProductFeatures.NewArrival,
+                "Этот букет состоит из нежных розово-белых альстромерий, " +
+                "которые создают гармоничное сочетание цветов. " +
+                "Зеленые листья и веточки добавляют свежести и живости. " +
+                "Букет аккуратно упакован в розовую бумагу, придавая ему элегантный вид. " +
+                "Такой букет станет отличным подарком для любого торжества или просто чтобы поднять настроение любимому человеку.");
+
+                CreateShopCard(
+                shopCardRepository,
+                productDescriptionRepository,
+                webRootPath,
+                "Сердце страсти",
+                "HolidayCollection12",
+                "HolidayCollection",
+                1900,
+                20,
+                ProductFeatures.DealOfDay,
+                "В этом букете красные розы собраны в форме сердца, " +
+                "что делает его идеальным подарком для выражения самых теплых и страстных чувств. " +
+                "Цветы аккуратно упакованы в двухцветную бумагу и перевязаны лентой, " +
+                "подчеркивающей яркость и романтичность композиции. " +
+                "«Сердце страсти» станет великолепным подарком на День святого Валентина, юбилей или любое другое важное событие.");
+
+                CreateShopCard(
+                shopCardRepository,
+                productDescriptionRepository,
+                webRootPath,
+                "Страстное Признание",
+                "HolidayCollection13",
+                "HolidayCollection",
+                2600,
+                30,
+                ProductFeatures.DealOfDay | ProductFeatures.Bestseller | ProductFeatures.NewArrival,
+                "Великолепная композиция из ярко-красных роз, элегантно завернутая в красно-белую бумагу. " +
+                "Этот букет излучает любовь и страсть, " +
+                "делая его идеальным подарком для выражения глубоких чувств.");
+
+                CreateShopCard(
+                shopCardRepository,
+                productDescriptionRepository,
+                webRootPath,
+                "Романтические Объятия",
+                "HolidayCollection14",
+                "HolidayCollection",
+                1700,
+                0,
+                ProductFeatures.NewArrival,
+                "Очаровательный букет в форме сердца из красных роз, " +
+                "дополненный натуральной зеленью и мешковиной. " +
+                "Эта очаровательная композиция символизирует любовь и романтику, " +
+                "идеально подходящая для особого человека.");
+
+                CreateShopCard(
+                shopCardRepository,
+                productDescriptionRepository,
+                webRootPath,
+                "Весенняя Гармония",
+                "HolidayCollection15",
+                "HolidayCollection",
+                1100,
+                5,
+                ProductFeatures.DealOfDay | ProductFeatures.Bestseller | ProductFeatures.NewArrival,
+                "Этот изысканный букет сочетает в себе нежные розовые гортензии и разноцветные гипсофилы. " +
+                "Упакованный в элегантную розовую бумагу, он приносит ощущение свежести и жизненной силы, " +
+                "идеально подходя для любого случая.");
+
+                #endregion
             }
         }
         private void CreateShopCard(
@@ -115,13 +320,13 @@ namespace FlowersShopMVCTraining.Repository
 
             if (File.Exists(sourceImagePath))
             {
-                
+
                 File.Copy(sourceImagePath, destinationImagePath, true);
             }
             else
             {
                 throw new FileNotFoundException($"Файл изображения '{sourceImagePath}' не найден.");
-            }   
+            }
         }
     }
 }

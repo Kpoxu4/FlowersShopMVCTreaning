14bb1784752ae575a5f1fa46b3676d16860fd1b5
Kpoxu4 <159006608+Kpoxu4@users.noreply.github.com>
2024-07-30 21:01:05 +0300

    clear input

--- FlowerIdeaSubmissionApi/Models/IdeaModel.cs
+++ FlowerIdeaSubmissionApi/Models/IdeaModel.cs
@@ -8,17 +8,17 @@ namespace FlowerIdeaSubmissionApi.Models
         private const string EROR_MESSAGE_PHONE = "Номер телефона должен состоять из 10–15 цифр и может начинаться с +.";
 
 
-        [Required(ErrorMessage = REQUIRED_EROR_MESSAGE)]
-        [StringLength(19, MinimumLength = 3, ErrorMessage = "Имя должно быть от 3 до 19 символов включая пробелы")]
+        //[Required(ErrorMessage = REQUIRED_EROR_MESSAGE)]
+        ////[StringLength(19, MinimumLength = 3, ErrorMessage = "Имя должно быть от 3 до 19 символов включая пробелы")]
         public string AuthorName { get; set; }
 
-        [Required(ErrorMessage = REQUIRED_EROR_MESSAGE)]
-        [StringLength(15, MinimumLength = 10, ErrorMessage = EROR_MESSAGE_PHONE)]
-        [RegularExpression(@"\+?\d{10,15}", ErrorMessage = EROR_MESSAGE_PHONE)]
+        //[Required(ErrorMessage = REQUIRED_EROR_MESSAGE)]
+        //[StringLength(15, MinimumLength = 10, ErrorMessage = EROR_MESSAGE_PHONE)]
+        //[RegularExpression(@"\+?\d{10,15}", ErrorMessage = EROR_MESSAGE_PHONE)]
         public int AuthorPhone { get; set; }
 
-        [Required(ErrorMessage = REQUIRED_EROR_MESSAGE)]
-        [StringLength(226, MinimumLength = 20, ErrorMessage = "Текст должен быть от 20 до 226 символов включая пробелы")]
+        //[Required(ErrorMessage = REQUIRED_EROR_MESSAGE)]
+        //[StringLength(226, MinimumLength = 20, ErrorMessage = "Текст должен быть от 20 до 226 символов включая пробелы")]
         public string Text { get; set; }    
     }
 }
--- FlowersShopMVCTreaning/wwwroot/js/submittingIdea.js
+++ FlowersShopMVCTreaning/wwwroot/js/submittingIdea.js
@@ -1,6 +1,6 @@
-$(document).ready(function () {
+$(document).ready(function () {    
     const baseApiUrl = `https://localhost:7078`;    
-    const button = $('.offer-your-bouquet-btn');
+    const button = $('.offer-your-bouquet-btn');     
 
     button.click(sendText);
 
@@ -9,6 +9,10 @@ $(document).ready(function () {
         const authorName = $('.offer-your-bouquet__name').val();
         const authorPhone = $('.offer-your-bouquet__phone').val();
         const idea = $('.offer-your-bouquet__idea').val();
+        const messageWindow = $('.window-message-for-user');
+        const messageContent = $('.window-message-for-user-content');
+        const messageText = $('.window-message-for-user-text');  
+
         const body = {
             authorName: authorName,
             authorPhone: authorPhone - 0,
@@ -21,6 +25,15 @@ $(document).ready(function () {
             contentType: 'application/json; charset=utf-8', 
             success: function (response) {
                 console.log('Nice');
+                if (response.message) {                         
+                    messageWindow.css("display", "flex"); 
+                    messageWindow.contents().show();
+                    messageText.text(response.message);  
+
+                    $('.offer-your-bouquet__name').val('');
+                    $('.offer-your-bouquet__phone').val('');
+                    $('.offer-your-bouquet__idea').val('');
+                }               
             },
             error: function (xhr, status, error) {
                 console.error(error);
